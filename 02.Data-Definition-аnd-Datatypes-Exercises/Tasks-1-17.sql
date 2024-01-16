514e9864da5c31e5f935fa3f7dea2b4628b765f8
Stefan Stoyanov <96234247+iamstefanstoyanov@users.noreply.github.com>
2024-01-16 14:18:39 +0000

    Update Tasks-1-17.sql

--- 02.Data-Definition-аnd-Datatypes-Exercises/Tasks-1-17.sql
+++ 02.Data-Definition-аnd-Datatypes-Exercises/Tasks-1-17.sql
@@ -13,4 +13,30 @@ CREATE TABLE towns (
     CONSTRAINT pk_town_id
     PRIMARY KEY (town_id)
 );
+ALTER TABLE towns
+DROP COLUMN town_id;
+
+ALTER TABLE towns
+ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY;
+
+ALTER TABLE minions
+ADD COLUMN town_id INT;
+
+ALTER TABLE minions
+ADD CONSTRAINT fk_town_id FOREIGN KEY (town_id) REFERENCES towns(id);
+
+INSERT INTO towns(name) VALUES 
+('Sofia'),
+('Plovdiv'),
+('Varna');
+
+INSERT INTO minions(name,age,town_id) VALUES 
+('Kevin', 22, 1),
+('Bob', 15, 3),
+('Steward', NULL, 2);
+
+TRUNCATE `minions`;
+
+DROP TABLE `minions`;
+DROP TABLE `towns`; 
 
