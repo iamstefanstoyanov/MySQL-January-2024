c9adc73527ea1964f5992b870b2117df5a60d422
Stefan Stoyanov <96234247+iamstefanstoyanov@users.noreply.github.com>
2024-01-16 12:10:52 +0000

    Create 01.CreateTables.sql

---
+++ 01.Data-Definition-аnd-Datatypes/01.CreateTables.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE gamebar;
+
+CREATE TABLE `gamebar`.`employees`(
+id INT PRIMARY KEY AUTO_INCREMENT,
+first_name VARCHAR(50) NOT NULL,
+last_name VARCHAR(50) NOT NULL
+);
+CREATE TABLE `gamebar`.`categories`(
+id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(50) NOT NULL
+);
+CREATE TABLE `gamebar`.`products`(
+id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(50) NOT NULL,
+category_id INT NOT NULL
+);
+
