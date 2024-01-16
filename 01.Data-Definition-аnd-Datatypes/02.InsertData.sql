fff95fcf9dfa6c183656c020c825516e6b840b3d
Stefan Stoyanov <96234247+iamstefanstoyanov@users.noreply.github.com>
2024-01-16 12:22:15 +0000

    Create 02.InsertData.sql

---
+++ 01.Data-Definition-аnd-Datatypes/02.InsertData.sql
@@ -0,0 +1,3 @@
+INSERT INTO employees(first_name,last_name) VALUES("Pesho","Pesho");
+INSERT INTO employees(first_name,last_name) VALUES("Gosho","Gosho");
+INSERT INTO employees(first_name,last_name) VALUES("Ivan","Ivan");
