8b2c43a2a2be8a40d2e5333b329a564d0a6f66c4
Stefan Stoyanov <96234247+iamstefanstoyanov@users.noreply.github.com>
2024-01-19 20:28:03 +0000

    Update Tasks-01-16.sql

--- 03.Built-in-Functions-Exercises/Tasks-01-16.sql
+++ 03.Built-in-Functions-Exercises/Tasks-01-16.sql
@@ -21,6 +21,40 @@ FROM `employees`
 WHERE LOWER(`job_title`) NOT LIKE '%engineer%'
 ORDER BY `employee_id`;
 
-SELECT `name` from `towns`
+SELECT `name` FROM `towns`
 WHERE length(`name`) IN ( 5, 6)
 ORDER BY `name`;
+
+SELECT `town_id`,`name`
+FROM `towns`
+WHERE SUBSTRING(`name` FROM 1 FOR 1) IN ('M', 'K', 'B', 'E')
+ORDER BY `name`;
+
+SELECT `town_id`,`name`
+FROM `towns`
+WHERE SUBSTRING(`name` FROM 1 FOR 1) NOT IN ('R', 'B', 'D')
+ORDER BY `name`;
+
+CREATE VIEW `v_employees_hired_after_2000` AS
+SELECT `first_name`, `last_name`
+FROM `employees`
+WHERE EXTRACT(YEAR FROM `hire_date`) > 2000;
+
+SELECT * FROM `v_employees_hired_after_2000`;
+
+SELECT `first_name`, `last_name` 
+FROM `employees`
+WHERE length(`last_name`) IN ( 5 );
+
+SELECT `country_name`, `iso_code`
+FROM `countries`
+WHERE LENGTH(`country_name`) - LENGTH(REPLACE(LOWER(`country_name`), 'a', '')) >= 3
+ORDER BY `iso_code`;
+
+SELECT `peak_name`, `river_name`, 
+LOWER(CONCAT(`peak_name`, SUBSTRING(`river_name`, 2))) 
+AS `mix`
+FROM `peaks`, `rivers`
+WHERE RIGHT(`peak_name`, 1) = LEFT(`river_name`, 1)
+ORDER BY `mix`;
+
