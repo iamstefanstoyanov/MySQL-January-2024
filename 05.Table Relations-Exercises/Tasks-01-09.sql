f15b355fee9ba5924d6503154844c99c48b6dab7
Stefan Stoyanov <96234247+iamstefanstoyanov@users.noreply.github.com>
2024-01-28 16:04:54 +0000

    Update Tasks-01-09.sql

--- 05.Table Relations-Exercises/Tasks-01-09.sql
+++ 05.Table Relations-Exercises/Tasks-01-09.sql
@@ -101,3 +101,93 @@ INSERT INTO teachers (teacher_id, name, manager_id) VALUES
 ALTER TABLE teachers
 ADD FOREIGN KEY (manager_id) REFERENCES teachers(teacher_id);
 
+CREATE TABLE cities (
+    city_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50)
+);
+
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    birthday DATE,
+    city_id INT,
+    FOREIGN KEY (city_id)
+        REFERENCES cities (city_id)
+);
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT,
+    FOREIGN KEY (customer_id)
+        REFERENCES customers (customer_id)
+);
+
+CREATE TABLE item_types(
+item_type_id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(50)
+);
+
+CREATE TABLE items (
+    item_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    item_type_id INT,
+    FOREIGN KEY (item_type_id)
+        REFERENCES item_types (item_type_id)
+);
+
+CREATE TABLE order_items (
+    order_id INT,
+    item_id INT,
+    PRIMARY KEY (order_id , item_id),
+    FOREIGN KEY (order_id)
+        REFERENCES orders (order_id),
+    FOREIGN KEY (item_id)
+        REFERENCES items (item_id)
+);
+
+CREATE TABLE majors (
+    major_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50)
+);
+CREATE TABLE students (
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    student_number VARCHAR(12),
+    student_name VARCHAR(50),
+    major_id INT,
+    FOREIGN KEY (major_id)
+        REFERENCES majors (major_id)
+);
+CREATE TABLE payments (
+    payment_id INT PRIMARY KEY AUTO_INCREMENT,
+    payment_date DATE,
+    payment_amount DECIMAL(8 , 2 ),
+    student_id INT,
+    FOREIGN KEY (student_id)
+        REFERENCES students (student_id)
+);
+
+CREATE TABLE subjects (
+    subject_id INT PRIMARY KEY AUTO_INCREMENT,
+    subject_name VARCHAR(50)
+);
+
+CREATE TABLE agenda (
+    student_id INT,
+    subject_id INT,
+    PRIMARY KEY (student_id , subject_id),
+    FOREIGN KEY (student_id)
+        REFERENCES students (student_id),
+    FOREIGN KEY (subject_id)
+        REFERENCES subjects (subject_id)
+);
+
+
+SELECT 
+    m.mountain_range, p.peak_name, p.elevation
+FROM
+    peaks p
+        JOIN
+    mountains m ON p.mountain_id = m.id
+WHERE
+    m.mountain_range = 'Rila'
+ORDER BY p.elevation DESC;
