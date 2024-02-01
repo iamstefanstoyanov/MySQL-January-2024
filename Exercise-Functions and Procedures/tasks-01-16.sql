c7c47a0e90da2962258dad559fbf6734fa839cc6
Stefan Stoyanov <96234247+iamstefanstoyanov@users.noreply.github.com>
2024-02-01 19:40:29 +0000

    Update tasks-01-16.sql

--- Exercise-Functions and Procedures/tasks-01-16.sql
+++ Exercise-Functions and Procedures/tasks-01-16.sql
@@ -83,3 +83,154 @@ BEGIN
 END//
 
 DELIMITER ; 
+
+DELIMITER //
+
+CREATE PROCEDURE usp_get_holders_full_name()
+BEGIN
+	SELECT CONCAT(first_name,' ',last_name) AS full_name
+    FROM account_holders
+    ORDER BY full_name, id;
+END//
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE usp_get_holders_with_balance_higher_than (target_salary DECIMAL (19,4))
+BEGIN
+	SELECT ah.first_name,ah.last_name FROM account_holders ah
+    JOIN accounts a ON ah.id = a.account_holder_id
+    WHERE target_salary < (SELECT SUM(balance) 
+FROM accounts
+WHERE account_holder_id = ah.id
+GROUP BY account_holder_id)
+GROUP BY ah.id
+ORDER BY ah.id;
+END//
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE FUNCTION ufn_calculate_future_value(sum DECIMAL(19, 4), interest_rate DOUBLE, years INT)
+RETURNS DECIMAL(19, 4)
+DETERMINISTIC
+RETURN sum*POW(1+interest_rate, years);
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE usp_calculate_future_value_for_account(id INT, interest_rate DECIMAL(19, 4))
+DETERMINISTIC
+BEGIN
+SELECT a.id, first_name, last_name, balance AS current_balance, ufn_calculate_future_value(balance, interest_rate, 5) AS balance_in_5_years
+FROM accounts a
+JOIN account_holders ah ON a.account_holder_id = ah.id
+WHERE a.id = id;
+END//
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE usp_deposit_money(account_id INT, money_amount DECIMAL(19, 4))
+BEGIN
+	START TRANSACTION;
+	IF((SELECT count(a.id) FROM accounts AS a WHERE a.id = account_id) != 1 OR money_amount < 0) THEN
+	ROLLBACK;
+	ELSE
+		UPDATE accounts 
+        SET balance = balance + money_amount
+		WHERE accounts.id = account_id;
+	END IF; 
+END
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE usp_withdraw_money(account_id INT, money_amount DECIMAL(19, 4))
+BEGIN
+	START TRANSACTION;
+	IF((SELECT count(a.id) FROM accounts a WHERE a.id = account_id) != 1 
+    OR money_amount < 0 
+    OR (SELECT balance FROM accounts a WHERE a.id = account_id) < money_amount) THEN
+	ROLLBACK;
+	ELSE
+		UPDATE accounts 
+        SET balance = balance - money_amount
+		WHERE accounts.id = account_id;
+	END IF; 
+END//
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE usp_transfer_money(from_account_id INT, to_account_id INT, amount DECIMAL(19, 4))
+BEGIN
+	START TRANSACTION;
+	IF((SELECT count(a.id) FROM accounts a WHERE a.id = to_account_id) != 1 
+    OR (SELECT count(a.id) FROM accounts a WHERE a.id = from_account_id) != 1 
+    OR amount < 0 
+    OR (SELECT balance FROM accounts a WHERE a.id = from_account_id) < amount
+    OR from_account_id = to_account_id
+    ) 
+    THEN
+	ROLLBACK;
+	ELSE
+		UPDATE accounts 
+        SET balance = balance - amount
+		WHERE accounts.id = from_account_id;
+UPDATE accounts 
+SET 
+    balance = balance + amount
+WHERE
+    accounts.id = to_account_id;
+	END IF; 
+END//
+
+CREATE TABLE `logs`(
+log_id INT AUTO_INCREMENT PRIMARY KEY,
+account_id INT,
+old_sum DECIMAL(19, 4),
+new_sum DECIMAL(19, 4)
+);
+
+DELIMITER //
+
+CREATE TRIGGER log_accounts_trigger BEFORE UPDATE ON accounts
+FOR EACH ROW
+BEGIN
+IF(OLD.balance != NEW.balance) THEN 
+INSERT INTO `logs` (account_id, old_sum, new_sum)
+VALUES (NEW.id, OLD.balance, NEW.balance); 
+END IF;
+END//
+
+DELIMITER ;
+
+CREATE TABLE notification_emails (
+id INT AUTO_INCREMENT PRIMARY KEY,
+recipient INT, 
+`subject` TEXT, 
+body TEXT);
+
+DELIMITER //
+
+CREATE TRIGGER log_logs_trigger AFTER INSERT ON `logs`
+FOR EACH ROW
+BEGIN
+INSERT INTO notification_emails (recipient, `subject`, body)
+SELECT account_id, 
+CONCAT('Balance change for account: ', account_id), 
+CONCAT('On ', DATE_FORMAT(NOW(), '%b %d %Y at %r'), ' your balance was changed from ', 
+ROUND(old_sum, 2), ' to ', ROUND(new_sum, 2))
+FROM `logs`
+ORDER BY log_id DESC 
+LIMIT 1; 
+END//
+
+DELIMITER ;
